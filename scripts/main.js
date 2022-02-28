70711ee98b1e0fb9b1d780a5585acba0ca1efc3c
Tadesse-Alemayehu <email24t@gmail.com>
2022-02-28 23:05:48 +0300

    Add event listener to display mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const mobileMenuBar = document.querySelector('#app-bar-mobile');
+    const logo = document.querySelector('.logo');
     const ListOfMenus = document.querySelector('#app-bar-menus');
-    const humBurger = mobileMenuBar.querySelector('.menu');
+    const toolBar = ListOfMenus.querySelector('#tool-bar');
+    const humBurger = mobileMenuBar.querySelectorAll('.menu-icon')[0];
+    const cancel = mobileMenuBar.querySelectorAll('.menu-icon')[1];
+    const returnHome = document.querySelector('#closeMenu');
+    const mobileMenuBarHeight = mobileMenuBar.scrollHeight;
+    const windowHeight = window.innerHeight;
     humBurger.addEventListener('click', () => {
         ListOfMenus.classList.toggle('display-none');
-        console.log("clicked");
+        ListOfMenus.classList.toggle('show');
+        logo.text = "";
+        ListOfMenus.style = `position: absolute; top:${mobileMenuBarHeight}px`;
+        toolBar.style = `min-height: ${windowHeight - mobileMenuBarHeight - 40}px`
+        humBurger.classList.add('display-none');
+        cancel.classList.remove('display-none');
+
     });
+
 });
