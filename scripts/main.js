6d7a93f16ec4b386596dcf33e1e80ab932283a94
Tadesse-Alemayehu <email24t@gmail.com>
2022-02-28 20:29:41 +0300

    Target all required attributes in .js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-const humBurger = document.querySelector('#app-bar-mobile').querySelector('.menu');
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuBar = document.querySelector('#app-bar-mobile');
+    const ListOfMenus = document.querySelector('#app-bar-menus');
+    const humBurger = mobileMenuBar.querySelector('.menu');
+    humBurger.addEventListener('click', () => {
+        ListOfMenus.classList.toggle('display-none');
+        console.log("clicked");
+    });
+});
