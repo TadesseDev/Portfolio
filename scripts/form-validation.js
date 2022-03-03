d86ae4ce0bd8b12d30eb2613ed9ac4a05f01dfcb
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-03 23:23:09 +0300

    localStorage

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -4,7 +4,8 @@ const addErrorMessage = (element, message) => {
 
 const appendSibling = (element, newSibling) => {
   element.parentNode.appendChild(newSibling);
-}
+};
+
 // using a function we can implement sophisticate email validation rules in the feature
 const validateEmail = (email) => {
   if (email.value !== email.value.toLowerCase()) {
@@ -20,16 +21,29 @@ const validateFormElements = (event) => {
   event.preventDefault();
   const form = event.target;
   const ERROR_MSG = "<b>Submission FAILS!!</b><br/>Letters to email should be all lower case.<br /> Like: 'example@mail.com'<br/>";
-  if (validateEmail(form.elements.email, small.cloneNode())) {
+  if (validateEmail(form.elements.email)) {
     form.submit();
   } else addErrorMessage(form, ERROR_MSG);
 };
 
+// generic form function's to store and update any from elements
+const updateFormElement = (storage, formElements) => {
+  formElements.forEach((element) => {
+    element.value = storage[element.getAttribute('id')];
+  });
+};
 
-// form validation will begin once DOM is ready
+const storeFormData = (storage, key, value) => {
+  const updateValue = JSON.parse(localStorage.getItem(storage));
+  updateValue[key] = value;
+  localStorage.setItem(storage, JSON.stringify(updateValue));
+};
+
+// function call will commence once DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
   const formSection = document.getElementById('contact-form');
   const form = formSection.querySelector('form');
+  const formElements = Array.from(form.elements);
   const small = document.createElement('small');
   appendSibling(form.elements.email, small);
   form.addEventListener('submit', (event) => validateFormElements(event));
@@ -40,18 +54,26 @@ document.addEventListener('DOMContentLoaded', () => {
       event.target.classList.add('input-fail');
     }
   });
-  if (localStorage.getItem('contactFormData') !== null) {
-    // console.log(JSON.parse(localStorage.getItem('contactFormData')).user_name);
-    // localStorage.removeItem('contactFormData');
 
-  }
-  else {
+  /* 
+  - check if Local storage is created 
+    - If so populate object with stored data and update the form with the stored data 
+    - If not prepare object with key and empty value;
+  */
+  let storage = {};
+  if (localStorage.getItem('contactFormData') !== null) {
+
+
+  } else {
     try {
-      localStorage.setItem('contactFormData', JSON.stringify({ user_name: '', email: '', message: '' }));
+      formElements.forEach((element) => {
+        if (element.hasAttribute('id')) {
+          storage[element.getAttribute('id')] = '';
+          element.addEventListener('input', () => storeFormData('contactFormData', element.getAttribute('id'), element.value));
+        }
+      });
+      localStorage.setItem('contactFormData', JSON.stringify(storage));
     } catch (ex) {
-      const error = small.cloneNode();
-      error.innerHTML = `Error saving form data to your device`
-      form.appendChild(error);
     }
   }
 });
