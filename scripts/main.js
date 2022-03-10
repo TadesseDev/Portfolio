c3a5808ed563e220c6fe718b207c76202e4894cf
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-10 20:30:55 +0300

    fix semantic and good practice Issues

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const mobileMenuBar = document.querySelector('#app-bar-mobile');
   const menu = mobileMenuBar.querySelector('.menu');
   const logo = document.querySelector('.logo');
-  const ListOfMenus = document.querySelector('#app-bar-menus');
-  const toolBar = ListOfMenus.querySelector('#tool-bar');
+  const listOfMenus = document.querySelector('#app-bar-menus');
+  const toolBar = listOfMenus.querySelector('#tool-bar');
   const humBurger = mobileMenuBar.querySelectorAll('.menu-icon')[0];
   const returnHome = document.querySelector('#closeMenu');
   const recentWorkModal = document.querySelector('#recent-work-modal');
@@ -14,29 +14,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // dispose mobile menu
   function disposeMobileMenu() {
-    ListOfMenus.classList.toggle('display-none');
-    ListOfMenus.classList.toggle('show');
+    listOfMenus.classList.toggle('hide');
+    listOfMenus.classList.toggle('show');
     logo.text = 'Welcome';
-    humBurger.classList.remove('display-none');
+    humBurger.classList.remove('hide');
     menu.lastChild.remove();
   }
 
   humBurger.addEventListener('click', () => {
-    ListOfMenus.classList.toggle('display-none');
-    ListOfMenus.classList.toggle('show');
+    listOfMenus.classList.toggle('hide');
+    listOfMenus.classList.toggle('show');
     logo.text = '';
-    ListOfMenus.style = `position: absolute; top:${mobileMenuBarHeight}px`;
-    toolBar.style = `min-height: ${windowHeight - mobileMenuBarHeight - 40}px`;
+    listOfMenus.setAttribute('style', `position: absolute; top:${mobileMenuBarHeight}px`);
+    toolBar.setAttribute('style', `min-height: ${windowHeight - mobileMenuBarHeight - 40}px`);
     const cancel = document.createElement('li');
     cancel.classList.add('menu-icon');
     const img = document.createElement('img');
     img.setAttribute('src', './images/icons/Cancel.svg');
     img.setAttribute('alt', 'close');
     cancel.appendChild(img);
-    humBurger.classList.add('display-none');
+    humBurger.classList.add('hide');
     menu.appendChild(cancel);
     cancel.addEventListener('click', disposeMobileMenu);
-    ListOfMenus.querySelectorAll('li').forEach((element) => {
+    listOfMenus.querySelectorAll('li').forEach((element) => {
       element.addEventListener('click', disposeMobileMenu);
     });
   });
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
   /* Portfolio: details popup window */
 
   const hideModal = () => {
-    recentWorkModal.classList.add('display-none');
+    recentWorkModal.classList.add('hide');
     recentWorkModal.innerHTML = `<div class="container"> 
     <span id="close-modal-desktop"></span> 
     <div id="feature">  </div> </div>`;
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
     recentWorkModal.querySelector('.container').appendChild(technology);
     recentWorkModal.querySelector('.container').appendChild(description);
     recentWorkModal.querySelector('.container').appendChild(links);
-    recentWorkModal.classList.remove('display-none');
+    recentWorkModal.classList.remove('hide');
     fullPage.setAttribute('style', `height: ${recentWorkModal.scrollHeight}px`);
     disposeDesktop.addEventListener('click', () => hideModal());
   };
