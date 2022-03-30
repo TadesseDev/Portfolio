18ed49a291c3adec7ac3f7628d48c0e914b2c914
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-30 21:22:42 +0300

    link live pages, github repos and images to each projects

--- scripts/main.js
+++ scripts/main.js
@@ -138,6 +138,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const featureImageType = window.innerWidth < 922 ? '-mobile.svg' : '.svg';
   const Projects = [
     {
+      name: 'To do lists',
+      title: 'To do list organizer',
+      description: `This is a single-page application (SPA) that you can use to organize your daily schedule. You can create a task ✍🏼, mark a task as completed ✔, update its details ⚒, or delete it 🚩. All this without needing to reload a page. ✔`,
+      featuredImage: `./images/pictures/project-snapshots/PNG/To-Do-list.PNG`,
+      featuredImageAlt: 'To-Do-list.PNG Image',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      liveVersion: 'https://tadesse-alemayehu.github.io/To-Do-list/',
+      source: 'https://github.com/Tadesse-Alemayehu/To-Do-list',
+    },
+    {
+      name: 'Awesome book project with ES6 syntax.',
+      title: 'Awesome book project with ES6 syntax.',
+      description: `Hello there  ✋🏼 . You like reading. here is an awesome single page  App to help you organize your books`,
+      featuredImage: `./images/pictures/project-snapshots/PNG/Awesome-Books-ES6.PNG`,
+      featuredImageAlt: 'Awesome-Books-ES6.PNG Image',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      liveVersion: 'https://tadesse-alemayehu.github.io/Awesome-Books-ES6/',
+      source: 'https://github.com/Tadesse-Alemayehu/Awesome-Books-ES6',
+    },
+    {
       name: 'Learn to code',
       title: 'Learn to code',
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius
@@ -148,7 +168,7 @@ document.addEventListener('DOMContentLoaded', () => {
       sapien eu velit porttitor semper. Aliquam sed elementum enim.
       Suspendisse ultrices quis enim at gravida. Ut lectus urna, cursus
       et tellus in, faucibus lacinia urna. Morbi nibh purus, vehicula at`,
-      featuredImage: `https://user-images.githubusercontent.com/69077061/157525702-966cc141-e7a1-4a41-99e1-d31efcfcaf57.png`,
+      featuredImage: `./images/pictures/project-snapshots/PNG/LEARN-TO-CODE.PNG`,
       featuredImageAlt: 'Learn to code project image',
       technologies: ['HTML', 'CSS', 'JavaScript'],
       liveVersion: 'https://tadesse-alemayehu.github.io/LEARN-TO-CODE/',
@@ -165,93 +185,35 @@ document.addEventListener('DOMContentLoaded', () => {
       sapien eu velit porttitor semper. Aliquam sed elementum enim.
       Suspendisse ultrices quis enim at gravida. Ut lectus urna, cursus
       et tellus in, faucibus lacinia urna. Morbi nibh purus, vehicula at`,
-      featuredImage: `./images/pictures/Snapshoot-Portfolio${featureImageType}`,
-      featuredImageAlt: 'Rock-Paper-Scissors Image',
+      featuredImage: `./images/pictures/project-snapshots/PNG/Rock-Paper-Scissors-Advance.PNG`,
+      featuredImageAlt: 'Rock-Paper-Scissors-Advance.PNG Image',
       technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-      liveVersion: 'https://winmac.ml/',
+      liveVersion: 'https://tadesse-alemayehu.github.io/Rock-Paper-Scissors-Advance/',
       source: 'https://github.com/Tadesse-Alemayehu/Rock-Paper-Scissors-Advance',
     },
     {
       name: 'Etch-A-Sketch',
       title: 'Project two(drawing board)',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius
-      turpis id metus vehicula, a faucibus neque vehicula. Etiam
-      tincidunt ante et dui efficitur ultricies. Nulla ex felis, mattis
-      eget lacinia sed, molestie at nisi. Nulla iaculis mi finibus augue
-      pharetra, quis pellentesque metus hendrerit. Vestibulum tristique
-      sapien eu velit porttitor semper. Aliquam sed elementum enim.
-      Suspendisse ultrices quis enim at gravida. Ut lectus urna, cursus
-      et tellus in, faucibus lacinia urna. Morbi nibh purus, vehicula at`,
-      featuredImage: `./images/pictures/Snapshoot-Portfolio${featureImageType}`,
-      featuredImageAlt: 'Etch-A-Sketch Image',
+      description: `So you love to draw? Good! This is an Etch-a-sketch project and you can create an awesome drawing board with your pre-defined pixel range and draw your picture.`,
+      featuredImage: `./images/pictures/project-snapshots/PNG/Etch-A-Sketch.PNG`,
+      featuredImageAlt: 'Etch-A-Sketch.PNG Image',
       technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
       liveVersion: 'https://tadesse-alemayehu.github.io/Etch-A-Sketch/',
       source: 'https://github.com/Tadesse-Alemayehu/Etch-A-Sketch',
     },
     {
-      name: 'Portfolio',
-      title: 'Project three',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius
-      turpis id metus vehicula, a faucibus neque vehicula. Etiam
-      tincidunt ante et dui efficitur ultricies. Nulla ex felis, mattis
-      eget lacinia sed, molestie at nisi. Nulla iaculis mi finibus augue
-      pharetra, quis pellentesque metus hendrerit. Vestibulum tristique
-      sapien eu velit porttitor semper. Aliquam sed elementum enim.
-      Suspendisse ultrices quis enim at gravida. Ut lectus urna, cursus
-      et tellus in, faucibus lacinia urna. Morbi nibh purus, vehicula at`,
-      featuredImage: `./images/pictures/Snapshoot-Portfolio${featureImageType}`,
-      featuredImageAlt: 'Portfolio Image',
-      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-      liveVersion: 'https://tadesse-alemayehu.github.io/Portfolio/',
-      source: 'https://github.com/Tadesse-Alemayehu/Portfolio',
-    },
-    {
-      name: 'faq-challenge',
-      title: 'Project four. Designed for FAQ chalenges',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius
-      turpis id metus vehicula, a faucibus neque vehicula. Etiam
-      tincidunt ante et dui efficitur ultricies. Nulla ex felis, mattis
-      eget lacinia sed, molestie at nisi. Nulla iaculis mi finibus augue
-      pharetra, quis pellentesque metus hendrerit. Vestibulum tristique
-      sapien eu velit porttitor semper. Aliquam sed elementum enim.
-      Suspendisse ultrices quis enim at gravida. Ut lectus urna, cursus
-      et tellus in, faucibus lacinia urna. Morbi nibh purus, vehicula at`,
-      featuredImage: `./images/pictures/Snapshoot-Portfolio${featureImageType}`,
-      featuredImageAlt: 'faq-challenge Image',
-      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-      liveVersion: 'https://tadesse-alemayehu.github.io/faq-challenge/',
-      source: 'https://github.com/Tadesse-Alemayehu/faq-challenge/tree/tadesse-optional',
-    },
-    {
       name: 'YouTube-Clone',
       title: 'Project five. Microverse Bootcamp',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius
-      turpis id metus vehicula, a faucibus neque vehicula. Etiam
-      tincidunt ante et dui efficitur ultricies. Nulla ex felis, mattis
-      eget lacinia sed, molestie at nisi. Nulla iaculis mi finibus augue
-      pharetra, quis pellentesque metus hendrerit. Vestibulum tristique
-      sapien eu velit porttitor semper. Aliquam sed elementum enim.
-      Suspendisse ultrices quis enim at gravida. Ut lectus urna, cursus
-      et tellus in, faucibus lacinia urna. Morbi nibh purus, vehicula at`,
-      featuredImage: `./images/pictures/Snapshoot-Portfolio${featureImageType}`,
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.In varius
+      turpis id metus vehicula, a faucibus neque vehicula.Etiam
+      tincidunt ante et dui efficitur ultricies.Nulla ex felis, mattis
+      eget lacinia sed, molestie at nisi.Nulla iaculis mi finibus augue
+      pharetra, quis pellentesque metus hendrerit.Vestibulum tristique
+      sapien eu velit porttitor semper.Aliquam sed elementum enim.
+      Suspendisse ultrices quis enim at gravida.Ut lectus urna, cursus
+      et tellus in, faucibus lacinia urna.Morbi nibh purus, vehicula at`,
+      featuredImage: `./images/pictures/project-snapshots/PNG/YouTube-Clone.PNG`,
       featuredImageAlt: 'YouTube-Clone Image',
-      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
-      liveVersion: 'https://tadesse-alemayehu.github.io/YouTube-Clone/',
-      source: 'https://github.com/Tadesse-Alemayehu/YouTube-Clone',
-    },
-    {
-      name: 'project-6',
-      title: 'My Recent Works',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius
-      turpis id metus vehicula, a faucibus neque vehicula. Etiam
-      tincidunt ante et dui efficitur ultricies. Nulla ex felis, mattis
-      eget lacinia sed, molestie at nisi. Nulla iaculis mi finibus augue
-      pharetra, quis pellentesque metus hendrerit. Vestibulum tristique
-      sapien eu velit porttitor semper. Aliquam sed elementum enim.
-      Suspendisse ultrices quis enim at gravida. Ut lectus urna, cursus
-      et tellus in, faucibus lacinia urna. Morbi nibh purus, vehicula at`,
-      featuredImage: `./images/pictures/Snapshoot-Portfolio${featureImageType}`,
-      featuredImageAlt: 'Rock-Paper-Scissors Image',
       technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
       liveVersion: 'https://tadesse-alemayehu.github.io/YouTube-Clone/',
       source: 'https://github.com/Tadesse-Alemayehu/YouTube-Clone',
