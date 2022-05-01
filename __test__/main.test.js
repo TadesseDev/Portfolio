e1093175827ce8960c55e871903754136754655e
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-01 20:33:12 +0300

    Add test for Return bar

--- __test__/main.test.js
+++ __test__/main.test.js
@@ -3,14 +3,20 @@ import data from '../scripts/modules/GLOBALS'
 import { DOMReadyActions } from '../scripts/main'
 DOMReadyActions();
 describe('Assert hamburger anc closeIcon are capable toggling menu options', () => {
-  test('assert hamburger hide the app-bar', () => {
+  test('assert hamburger show the menubar', () => {
     expect(String(data.listOfMenus.classList)).toBe('hide');
     data.humBurger.click();
     expect(String(data.listOfMenus.classList)).toBe('show');
   });
-  test('Assert close icon shows the app-bar', () => {
+  test('Assert close icon hide the menubar', () => {
     expect(String(data.listOfMenus.classList)).toBe('show');
     data.closeMobileMenu.click();
+    expect(String(data.listOfMenus.classList)).toBe('hide');
+  });
+  test('Assert returnHome icon hide the menubar', () => {
+    data.humBurger.click();
+    expect(String(data.listOfMenus.classList)).toBe('show');
+    data.returnHome.click();
     expect(String(data.listOfMenus.classList)).toBe('hide');
   });
 });
