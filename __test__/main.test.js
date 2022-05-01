0d979dd94fec0689ebdff5a3015d6488467dda06
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-01 15:17:41 +0300

    Install babel for jest

--- __test__/main.test.js
+++ __test__/main.test.js
@@ -1,2 +1,9 @@
-import projectData from '../scripts/modules/GLOBALS'
-import 
+// import projectData from '../scripts/modules/GLOBALS'
+import { domEvent } from '../scripts/main';
+// import { disposeMobileMenu } from '../scripts/modules/Functions';
+document.body.innerHTML = '../index.html';
+describe('Test initial dom content as dom loaded', () => {
+  test('test one', () => {
+    expect(domEvent()).toBe('abc');
+  });
+});
