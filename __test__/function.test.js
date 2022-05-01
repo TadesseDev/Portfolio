436d1036b1f839abc52ada59167f125f5b2d03a3
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-01 21:56:59 +0300

    Test navigation buttons for intial section link

--- __test__/function.test.js
+++ __test__/function.test.js
@@ -1,7 +1,8 @@
 
 import '../mock/initialDOM'
 import data from '../scripts/modules/GLOBALS'
-import { renderProjectToTheDom } from '../scripts/modules/Functions'
+import { renderProjectToTheDom, DOMReadyActions } from '../scripts/modules/Functions'
+DOMReadyActions();
 renderProjectToTheDom();
 const projectContainerNod = document.querySelector('#recent-work').querySelector('.card-flow');
 describe('Assert project list loading', () => {
@@ -9,6 +10,7 @@ describe('Assert project list loading', () => {
     expect(projectContainerNod.childElementCount).toBe(data.Projects.length)
   });
 });
+
 describe('Assert hide and show modal (project detail)', () => {
   const projectCard = projectContainerNod.querySelectorAll('.card');
   projectCard.forEach(card => {
@@ -20,5 +22,12 @@ describe('Assert hide and show modal (project detail)', () => {
     closeButton.click();
     expect(String(data.recentWorkModal.classList)).toBe('hide');
   })
-  // console.log(projectButtons.length)
 });
+
+describe('Assert navigation buttons are capable of changing section', () => {
+  test('Assert initial links for the navigation buttons', () => {
+    expect(data.goUp.href.split("#")[1]).toBe(data.sections[0].getAttribute('id'));
+    expect(data.godown.href.split("#")[1]).toBe(data.sections[1].getAttribute('id'));
+  });
+  // data.godown
+})
--- __test__/initial.test.js
+++ __test__/initial.test.js
@@ -2,11 +2,11 @@
 import '../mock/initialDOM'
 describe('Test initial dom content', () => {
   const section = document.querySelectorAll('section');
-  test('assert all section are present inside the dom', () => {
+  test('Assert all section are present inside the DOM', () => {
     expect(section.length).toBe(5);
   });
 
-  test('Assert menu element to toggle menu presents', () => {
+  test('Assert menu element for toggling menu presents', () => {
     const menuElement = document.querySelector('#app-bar-mobile').querySelector('.menu');
     const visibleMenu = menuElement.querySelectorAll('.hide');
     expect(menuElement).toBeTruthy();
