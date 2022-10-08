5a361ff8a07e0989775b55dbd79f17b21c8a64b2
Tadesse Alemayehu <tadalem26@gmail.com>
2022-10-08 15:05:16 +0300

    Remove: unused scripts

--- main.js
+++
@@ -1,6 +0,0 @@
-import { DOMReadyActions, renderProjectToTheDom } from './modules/Functions.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  DOMReadyActions();
-  renderProjectToTheDom();
-});
